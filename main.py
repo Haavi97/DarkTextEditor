d5da71d75dec056e45db983c7b18a2878c6224b8
fortin <fortin@ttu.ee>
2021-01-06 09:18:07 +0200

    Minimal working QT app

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ from PyQt5.QtWidgets import QWidget
 
 app = QApplication(sys.argv)
 
+window = QWidget()
+window.setWindowTitle('Dark Theme Text Editor')
+window.setGeometry(100, 100, 280, 80)
+window.move(60, 15)
+helloMsg = QLabel('<h1>Dark Theme Text Editor</h1>', parent=window)
+helloMsg.move(60, 15)
+
+window.show()
+
+sys.exit(app.exec_())
