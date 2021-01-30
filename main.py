db659b486a342a54b45beec54792b22450f76243
fortin <fortin@ttu.ee>
2021-01-30 19:37:47 +0200

    Changed to OOP. Added menu, tools, and status bar

--- main.py
+++ main.py
@@ -3,36 +3,50 @@ import time
 import datetime as dt
 
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QLabel, QTextEdit
 from PyQt5.QtWidgets import QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QToolBar
+from PyQt5.QtGui import QColor
 # from PyQt5.QtWidgets import * 
 from PyQt5 import QtCore 
 from PyQt5 import QtGui 
 
-app = QApplication(sys.argv)
 
-app.setWindowIcon(QtGui.QIcon('blackicon.svg')) 
+whiteColor = QColor(255, 255, 255)
+blackColor = QColor(0, 0, 0)
 
-window = QWidget()
-window.setWindowTitle('Dark Theme Text Editor')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Dark Theme Text Editor</h1>', parent=window)
-helloMsg.move(60, 15)
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Dark Theme Text Editor')
+        self.setCentralWidget(QTextEdit())
+        self.setStyleSheet("background-color: rgb(0, 0, 0); color: rgb(255,255,255);")
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
 
-window.show()
+    def _createMenu(self):
+        self.menu = self.menuBar().addMenu("&Menu")
+        self.menu.addAction('&Exit', self.close)
 
-centerPoint = QDesktopWidget().availableGeometry().center()
-window2 = QWidget()
-window2.setWindowTitle('Extra window')
-window2.setGeometry(100, 100, centerPoint.x(), centerPoint.y()*2)
-window2.move(centerPoint.x(),0)
-helloMsg2 = QLabel('<p>p text</p>', parent=window2)
-helloMsg2.move(60, 15)
+    def _createToolBar(self):
+        tools = QToolBar()
+        self.addToolBar(tools)
+        tools.addAction('Exit', self.close)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage('New file')
+        self.setStatusBar(status)
 
 
 if __name__ == "__main__":
-  
-    window2.show()
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QtGui.QIcon('blackicon.svg')) 
+
+    win = Window()
+    win.show()
 
     sys.exit(app.exec_())
