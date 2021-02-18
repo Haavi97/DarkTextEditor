872d6583505ec4e37e451ffb9885f28806949afd
fortin <fortin@ttu.ee>
2021-02-18 18:28:20 +0200

    About page added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import datetime as dt
+import threading
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel, QTextEdit
@@ -9,23 +10,28 @@ from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QStatusBar
 from PyQt5.QtWidgets import QToolBar
 from PyQt5.QtGui import QColor
-# from PyQt5.QtWidgets import * 
-from PyQt5 import QtCore 
-from PyQt5 import QtGui 
+# from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5 import QtGui
 
 
 whiteColor = QColor(255, 255, 255)
 blackColor = QColor(0, 0, 0)
+
+style = "background-color: rgb(0, 0, 0); color: rgb(255,255,255);"
+about_file = "about.html"
+
 
 class Window(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle('Dark Theme Text Editor')
         self.setCentralWidget(QTextEdit())
-        self.setStyleSheet("background-color: rgb(0, 0, 0); color: rgb(255,255,255);")
+        self.setStyleSheet(style)
         self._createMenu()
         self._createToolBar()
         self._createStatusBar()
+        self.about_window = About()
 
     def _createMenu(self):
         self.menu = self.menuBar().addMenu("&Menu")
@@ -41,16 +47,25 @@ class Window(QMainWindow):
     def _createStatusBar(self):
         self.status = QStatusBar()
         self.status.showMessage('New file')
-        self.setStatusBar(status)
+        self.setStatusBar(self.status)
 
     def about(self):
-        # TODO! Create an about window refering to the Github repo
-        print('about')
+        self.about_window.show()
 
+
+class About(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('About page')
+        f = open(about_file, "r")
+        self.about_text = f.read()
+        f.close()
+        self.setCentralWidget(QLabel(self.about_text))
+        self.setStyleSheet(style)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon('blackicon.svg')) 
+    app.setWindowIcon(QtGui.QIcon('blackicon.svg'))
 
     win = Window()
     win.show()
